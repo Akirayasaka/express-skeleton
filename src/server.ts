44e54767df5f09619ee0f1183c0d3c8719e7e0ee
Akira <kuramal12@msn.com>
2023-03-16 09:21:02 +0800

    Add CORS

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import type { Request, Response } from 'express';
+import cors from 'cors';
 import { routerList } from './routes';
 
 const app = express();
@@ -6,6 +8,22 @@ const port = 3000;
 
 app.use(express.json());
 // app.use(express.urlencoded({ extended: false }));
+
+//#region 解決跨域問題
+app.use(
+  cors({
+    origin: ['http://localhost:8080'],
+    methods: ['GET', 'POST'],
+  })
+);
+app.all('*', (req: Request, res: Response, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  next();
+});
+//#endregion
+
 routerList(app);
 
 app.listen(port, () => {
